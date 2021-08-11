1b627835b8e5fb0407ba53097c00807841916a2f
NUR ARIF HUSAINI BIN NORWAZA <chemist.arif.husaini@gmail.com>
2021-08-11 19:30:49 +0800

    The Permissions System

--- ebooksapi/ebooks/api/views.py
+++ ebooksapi/ebooks/api/views.py
@@ -1,36 +1,57 @@
 from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.exceptions import ValidationError
 # from rest_framework import mixins
 from rest_framework.generics import get_object_or_404
 
 from ebooks.models import Ebook, Review
 
 from ebooks.api.serializers import EbookSerializer, ReviewSerializer
-
+from ebooks.api.permissions import (
+    IsAdminUserOrReadOnly,
+    IsReviewAuthorOrReadOnly
+)
 
 class EbookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class EbookDetailApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class ReviewCreateAPIView(generics.CreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     # Override for customizations
     def perform_create(self, serializer):
         ebook_pk = self.kwargs.get("ebook_pk")
-        ebook  = get_object_or_404(Ebook, pk=ebook_pk)
-        serializer.save(ebook=ebook)
+        ebook = get_object_or_404(Ebook, pk=ebook_pk)
+
+        review_author = self.request.user
+
+        review_queryset = Review.objects.filter(
+            ebook=ebook,
+            review_author=review_author
+        )
+
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this ebook!")
+
+        serializer.save(ebook=ebook, review_author=review_author)
 
 
 class ReviewDetailApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsReviewAuthorOrReadOnly]
 
 # class EbookListCreateAPIView(
 #     mixins.ListModelMixin,
